38b047ac0e626b4ce34780589e7dfa13585f69ce
Huy <Huyphan1232203@gmail.com>
2025-06-01 08:16:09 +0700

    init

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,66 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context"; // Thêm import này
+
+import { HapticTab } from "@/components/HapticTab";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import Fontisto from "@expo/vector-icons/Fontisto";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+  const insets = useSafeAreaInsets(); // Thêm dòng này
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            position: "absolute",
+          },
+          android: {
+            // Thêm padding cho Android
+            paddingTop: insets.top,
+            height: 60 + insets.top, // Điều chỉnh height nếu cần
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Report"
+        options={{
+          title: "Report",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="notes-medical" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Setting"
+        options={{
+          title: "Setting",
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="player-settings" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
